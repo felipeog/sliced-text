378a2ff0f82ce3487c551c7a8278fac40e54060b
felipeog <17603069+felipeog@users.noreply.github.com>
2024-06-16 00:08:36 -0300

    uncomment code

--- js/index.js
+++ js/index.js
@@ -3,8 +3,6 @@ import { render } from "./_rendering.js";
 import { state } from "./_state.js";
 
 window.addEventListener("load", () => {
-  // createGui();
-  // state.animationFrameId = requestAnimationFrame(render);
-
-  requestAnimationFrame(render);
+  createGui();
+  state.animationFrameId = requestAnimationFrame(render);
 });
